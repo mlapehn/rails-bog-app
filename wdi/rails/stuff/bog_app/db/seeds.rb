7849b29becb6dabf440a3810477669c0aeeda91c
Matthew John Lapehn <mlapehn@gmail.com>
2016-07-27 11:29:02 -0600

    First commit with working index

---
+++ wdi/rails/stuff/bog_app/db/seeds.rb
@@ -0,0 +1,2 @@
+Creature.create({name: "Gollum", description: "originally known as Sméagol, was at first a Stoor, one of the three early Hobbit-types. Deformed and twisted in body and mind by the corruption of the Ring, his chief desire was to possess the very Ring that had enslaved him."})
+Creature.create({name: "Swamp Thing", description: "A humanoid mass of vegetable matter who fights to protect his swamp home, the environment in general, and humanity from various supernatural or terrorist threats."})
